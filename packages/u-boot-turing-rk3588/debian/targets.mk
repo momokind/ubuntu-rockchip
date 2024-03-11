c5446502c00a7b734a2fae64ac2e7ec5f22bc9e4
momokind <momokind2002@gmail.com>
2024-03-11 07:19:37 +0000

    Add board Sunniwell z96a laptop

--- packages/u-boot-turing-rk3588/debian/targets.mk
+++ packages/u-boot-turing-rk3588/debian/targets.mk
@@ -15,3 +15,8 @@ u-boot-rockchip_platforms += radxa-zero3-rk3566
 radxa-zero3-rk3566_ddr := rk3566_ddr_1056MHz_v1.18.bin
 radxa-zero3-rk3566_bl31 := rk3568_bl31_v1.43.elf
 radxa-zero3-rk3566_pkg := radxa-zero3
+
+u-boot-rockchip_platforms += z96a-rk3568
+z96a-rk3568 := rk3568_ddr_1560MHz_v1.18.bin
+z96a-rk3568_bl31 := rk3568_bl31_v1.43.elf
+z96a-rk3568_pkg := sunniwell-z96a
