c0c16f4e9d0cc54fe699735a4bf4e18da242dfa2
momokind <momokind2002@gmail.com>
2024-03-12 11:21:27 +0800

    Update targets.mk

--- packages/u-boot-turing-rk3588/debian/targets.mk
+++ packages/u-boot-turing-rk3588/debian/targets.mk
@@ -17,6 +17,6 @@ radxa-zero3-rk3566_bl31 := rk3568_bl31_v1.43.elf
 radxa-zero3-rk3566_pkg := radxa-zero3
 
 u-boot-rockchip_platforms += z96a-rk3568
-z96a-rk3568 := rk3568_ddr_1560MHz_v1.18.bin
+z96a-rk3568_ddr := rk3568_ddr_1560MHz_v1.18.bin
 z96a-rk3568_bl31 := rk3568_bl31_v1.43.elf
 z96a-rk3568_pkg := sunniwell-z96a
