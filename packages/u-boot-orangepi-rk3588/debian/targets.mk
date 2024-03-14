01493a4730654ec6a0b5d5dba88225f989b3e04a
momokind <momokind2002@gmail.com>
2024-03-14 11:58:23 +0000

    Add rk3568-z96a

--- packages/u-boot-orangepi-rk3588/debian/targets.mk
+++ packages/u-boot-orangepi-rk3588/debian/targets.mk
@@ -19,3 +19,8 @@ u-boot-rockchip_platforms += orangepi_5_plus
 orangepi_5_plus_ddr := rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.11.bin
 orangepi_5_plus_bl31 := rk3588_bl31_v1.38.elf
 orangepi_5_plus_pkg := orangepi-5-plus
+
+u-boot-rockchip_platforms += rk3568
+rk3568_ddr := rk3568_ddr_1560MHz_v1.18.bin
+rk3568_bl31 := rk3568_bl31_v1.43.elf
+rk3568_pkg := sunniwell-z96a
--- packages/u-boot-sunniwell-rk3568/debian/targets.mk
+++
@@ -1,7 +0,0 @@
-# Target platforms supported by u-boot.
-# debian/rules includes this Makefile snippet.
-
-u-boot-rockchip_platforms += rk3568
-rk3568_ddr := rk3568_ddr_1560MHz_v1.18.bin
-rk3568_bl31 := rk3568_bl31_v1.43.elf
-rk3568_pkg := sunniwell-z96a
